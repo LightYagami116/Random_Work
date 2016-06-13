a410fc1c721a908448a2cdbdfc597cce8ceb164a
Jonathan Allen <wastefuldragon@gmail.com>
2016-06-12 23:39:49 -0400

    completed

--- script.js
+++ script.js
@@ -10,7 +10,26 @@ var main = function(){
       $input.val("");
     }
 
-    //return false;
+    return false;
   });
 }
 $(document).ready(main);
+/*var main = function() {
+
+    $('form').submit(function(event) {
+        var $input = $(event.target).find('input');
+        var comment = $input.val();
+
+        if (comment != "") {
+            var html = $('<li>').text(comment);
+            html.prependTo('#comments');
+            $input.val("");
+        }
+
+        return false;
+    });
+
+}
+
+$(document).ready(main);
+*/
